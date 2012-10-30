5bc74bf868ef49505271ec6cb498a28274aba780
Daniel M Klein <kleindan@ampersand.seas.upenn.edu>
2012-10-30 19:08:27 -0400

    Commited a test file for the earlier multilined statement laden file

---
+++ ex/multiTests.py
@@ -0,0 +1,29 @@
+__author__ = 'Dan'
+
+import multiLineArgs_instrumented as multiLineArgs
+
+from RuntimeOracle import RuntimeOracle
+
+R = RuntimeOracle("euclid_passfail.txt")
+multiLineArgs.R = R
+
+R.assertTrue( multiLineArgs.isEqual(3, 3) ) # Should pass
+R.assertTrue( multiLineArgs.isEqual(3, 4) ) # Should fail
+
+R.assertTrue( multiLineArgs.isSameMagnitude(3, -3) ) # should pass
+R.assertTrue( multiLineArgs.isSameMagnitude(3, 3) )  # should pass
+R.assertTrue( multiLineArgs.isSameMagnitude(4, -3) ) # should fail
+R.assertTrue( multiLineArgs.isSameMagnitude(3, 4) )  # should fail
+
+m1 = multiLineArgs.mean(2, 2, 3, 3); # should be 2.5
+m2 = multiLineArgs.mean(2, 3, 3, 4); # should be 3.0
+
+s1 = multiLineArgs.stringConcat("This ", "is ", "a test.")  # Should be "This is a test"
+s2 = multiLineArgs.stringConcat("ML", "ALG", "SE")          # Should be "MLALGSE"
+
+R.assertTrue( m1 == 2.50 )  # should pass
+R.assertTrue( m2 == 3.01 )  # should fail
+
+R.assertTrue( s1 is "This is a test.")  # should pass
+R.assertTrue( s2 is "mlalgse")          # should fail
+
