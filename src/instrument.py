361a4c3873c5378df92a1682f984b0cd6a6d477f
Mark D. Moeller <mark.d.moeller@gmail.com>
2012-10-31 12:21:57 -0400

    Fixed instrument.py to handle classes

--- src/instrument.py
+++ src/instrument.py
@@ -4,15 +4,101 @@
 # author: Dan Klein
 # modified: Mark Moeller 24 Oct.
 # modified: Samarth Kishore 10/30/2012
+# modified: Mark 31 Oct.
 
 import re, sys
 
-def coupleLine(line,lineNum):
+
+
+# This function decides whether 'line' is the start of a new block. It returns one of 3 values:
+#   returns True if this line is a def blockhead
+#   returns False if this line is something else
+def isNonDefBlockHead(line):
+
+    # Not a blockhead
+    if ':' not in line:
+        return False
+
+    line_end = line.split(':')[-1].strip()
+
+    # This means it is a block:
+    if line_end == '' or line_end[0] == '#':
+        if len(line.strip()) > 3 and line.strip()[:3] == "def":
+            return False # is a def
+        else:
+            return True # is a non-def blockhead!
+
+    # Just happens to have a colon in it. Not a blockhead.
+    return False
+
+
+# We'll use indentLevel to help determine which lines need to be instrumented
+# In particular, we use it make classes work:
+#     if there is a class def on the baseline, then we shouldn't instrument level 1 as long
+#     as we are inside that class, etc.
+def indentLevel(line):
+    """Returns an int >= 0 how far indented this line is if it has non-comment contents, or -1 for whitespace or comment"""
     
-    # ignores (and removes!) comments and whitespace
-    if(len(line.strip()) == 0 or line.strip()[0] == '#'): return ""
-    # ignores function headers and else branches and instruments only imports on the baseline
-    if(line[0].isspace() is bool("") or line.strip() == "else:"): return "\n" + line
+    # Whitespace or total comment
+    if line.strip() == "" or line.strip()[0] == '#':
+        return -1
+
+    # If we are using tab indentation
+    if line[0] == '\t': 
+        count = 1
+        while line[count] == '\t':
+            count += 1
+
+    # Space indentation (this will go Array-OOB if the input is not valid python)
+    elif line[0] == ' ':
+        count = 1
+        while line[count*4] == ' ':
+            count += 1
+
+    # NO indentation, meaning it is baseline
+    else:
+        return 0
+
+    return count
+
+def coupleLine(line, lineNum, do_not_instrument_lvl = 0):
+
+    current_level = indentLevel(line)
+    block = isNonDefBlockHead(line)
+
+    # DEBUGGING:
+    # print "[dni: ", do_not_instrument_lvl, "] [level: ", current_level, "]", "[block: ", block, "]",  line
+
+    # If we are already instrumenting, keep instrumenting
+    if current_level > do_not_instrument_lvl:
+        newDNILvl = do_not_instrument_lvl
+
+    # Comment or whitespace
+    elif current_level == -1:
+        return ("", do_not_instrument_lvl)
+
+    # We need to see if this is a new block, and we can start instrumenting
+    elif current_level == do_not_instrument_lvl:
+
+        # Non-def block
+        if block:
+            return('\n' + line, do_not_instrument_lvl + 1)
+
+        return('\n' + line, do_not_instrument_lvl)
+
+    # We have returned from a higher level, do not instrument at this level.
+    # This is what ignores and removes comments and spaces.
+    else:
+        return ('\n' + line, current_level)
+    
+
+    # Finally, this is where we instrument:
+
+    # Special case for "else" statements
+    # NOTE: This assumes no statements in-line the else clause, which is technically valid python,
+    #     but considered bad style, so we will not accept it for now.
+    if line.strip() == "else:": 
+        return ("\n" + line, newDNILvl)
 
     # grabs all of the tabs associated with the line via regex
     spaces = re.findall(r'^\s*', line)
@@ -21,7 +107,7 @@ def coupleLine(line,lineNum):
     header = "\n" + spaces[0] + "f.write(str(" + str(lineNum+1) + ") + \'\\t\' + str(R.getRunNum()) + \'\\n\')"
     header += "\n" + line
 
-    return  header
+    return  (header, newDNILvl)
 
 def makeInstrumFile(path):
 
@@ -41,11 +127,18 @@ def makeInstrumFile(path):
     x=0
     flag=0
 
+    # do not instrument level
+    dni_level = 0 
+
     # for loop injects the instrumenting lines into the old source code
     for i in range (len(lines)):
     #instrument a file write only if it is a new statement (not a continuation)
-        if x==0 and flag==0:
-            newSource = newSource + coupleLine(lines[i], i)
+        
+        if x==0 and flag==0: 
+            couple = coupleLine(lines[i], i, do_not_instrument_lvl = dni_level)
+            newSource = newSource + couple[0]
+            dni_level = couple[1]
+
         else: 
             newSource = newSource +  lines[i]
         
@@ -64,9 +157,9 @@ def makeInstrumFile(path):
         """
         for j in range (len(lines[i])):
             if (lines[i][j]=="(" or lines[i][j]=="{" or lines[i][j]=="[" ):
-		 x=x+1
-	    if (lines[i][j]==")" or lines[i][j]=="}" or lines[i][j]=="]" ):
-		 x=x-1
+                x = x + 1
+            if (lines[i][j]==")" or lines[i][j]=="}" or lines[i][j]=="]" ):
+                x = x - 1
 
     # initializes and writes the instrumented code
     newFile = open( fileName + "_instrumented.py", 'w')
