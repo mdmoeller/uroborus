a8324adad5bc4d2cc7c64b50bf96d43fb06f93bc
Twisha Shah <twisha@seas.upenn.edu>
2012-11-13 11:04:46 -0500

    Use Display_Result.java instead of CodeChecker

--- src/CreateHtml.java
+++ src/CreateHtml.java
@@ -7,50 +7,63 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-/**
- *
 
- */
 public class CreateHtml {
  
-    public static void create(String code[],int col[])
+    public static void create(String code[],int col[], int stpf[][], double bright[])
     {
   // Create file 
-  FileWriter fstream;
+ 	 FileWriter fstream;
         try {
             fstream = new FileWriter("Data.html");
         
-  BufferedWriter out = new BufferedWriter(fstream);
-  out.write("<html><head><title>DATA</title></head>");
-  out.write("<body>");
-  out.write("<h1>CODE<h1>");
-  out.write("<form>");
+ 	 BufferedWriter out = new BufferedWriter(fstream);
+  	out.write("<html><head><title>DATA</title></head>");
+  	out.write("<body>");
+  	out.write("<h1><A HREF=\"\"onMouseOver=\"document.bgColor=\'white\'\">uroborus</a><h1>");
+  
   
  
-  String str=null;
-String shade=null;
-//String red="red";
-String sentence=null;
-    for( int i=1;i<code.length;i++)
+  	String str=null;
+	String shade=null;
+
+	String sentence=null;
+    	for( int i=1;i<code.length;i++)
         {
+        
+    		
+    	
+           str=code[i]; //+(col[i]/120);
             
-            str=code[i];
-            shade=RangeColor.htmlColor(col[i]);
-            //out.write("<font color='"+shade+"'>"+str+"</font><br/>");
- sentence="<INPUT TYPE='text' SIZE='"+30+"' STYLE='background-color:"+shade+"' value='"+str+" '  readonly><br/>"; 
-           out.write(sentence); 
-            //str.length()+
-        }
+           if(stpf[i][0]==0 && stpf[i][1]==0)
+         	sentence="<FONT size=\"5\" STYLE=\"background-color:white\" >"+str+"</FONT><br>";
+            	         else
+            	{	
+            	System.out.println("Color value= "+col[i]+"	Brightness value= "+bright[i]);
+            	shade=RangeColor.htmlColor(col[i], bright[i]); 
+            
+            	sentence="<FONT size=\"5\" STYLE=\"background-color:"+shade+"\" >"+str+" </FONT><br/>"; 
+   
+            	sentence="<A onMouseOver=\"document.bgColor=\'"+shade+"\'\">"+str+"</a><br>";
+            	}        
+            
+            	out.write(sentence); 
+           
+    		
+        }	
     
   
-   out.write("</body></html>");
+    
+    
+   	out.write("</body></html>");
   
-  //Close the output stream
-  out.close();
-  } catch (IOException ex) {
+  	//Close the output stream
+  	out.close();
+  	} 
+	catch (IOException ex) {
             Logger.getLogger(CreateHtml.class.getName()).log(Level.SEVERE, null, ex);
         }
   }
  
-  }
+ }
 
