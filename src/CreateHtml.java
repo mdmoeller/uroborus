bb5567ae58fc8283e123346d08ab2a17ba7eeead
Twisha Shah <twisha@seas.upenn.edu>
2012-12-03 23:39:35 -0500

    Text field and validation added

--- src/CreateHtml.java
+++ src/CreateHtml.java
@@ -1,4 +1,4 @@
-// package abc;
+package abc;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -20,51 +20,70 @@ public class CreateHtml {
         try {
         	String html_file=file_name+"_report.html";
             fstream = new FileWriter(html_file);
-            System.out.println("HTML file created!");
+            System.out.println("Output file created!");
         
   BufferedWriter out = new BufferedWriter(fstream);
-  out.write("<html><head><title>DATA</title></head>");
+  out.write("<html><head><title>Fault localization</title>");
+  out.write("<script language=\"javascript\" type=\"text/javascript\">");
+  out.write(" function validate(){ ");  
+  out.write("var b=form1.threshold.value;");
+  
+  out.write("var numbers = /^[0-9]+$/;  ");
+ out.write(" if(b.match(numbers))  ");
+   //out.write("    { return true;}");  
+   out.write("{ var c=parseInt(b);");
+   out.write("if(c<0 || c>120)");
+    out.write("   {alert(\"Please enter a number in the range [0,120]!\"); }}"); //}</script></head>"); 
+        out.write("else {alert(\"Please enter a valid number!\");}}");
+        out.write("</script></head>");
   out.write("<body>");
-  out.write("<h1><u>uroborus</u><h1><pre>");
-  //out.write("<form>");
+  out.write("<h1><u>uroborus</u></h1><pre>");
+  out.write("<form name=\"form1\" action=\"#\">");
+  out.write("<div align=\"center\">Threshold value: <input type=\"text\" name=\"threshold\"><br><input type=\"submit\" onclick=\"validate()\" value=\"Change\" /><br></div>");
+  
+  //out.write("<FONT size=\"5\" STYLE=\"background-color:#A4A4A4\" >Comments</font><br><br><br>");
   //out.write("<center>");
   
  
   String str=null;
 String shade=null;
-//String red="red";
+
 String sentence=null;
     for( int i=1;i<code.length;i++)
         {
         
-    		/*for(int j=0;j<blank.length;j++)
-    			if(i==blank[j])
-    				i++;
+    		
     	
-    		if(i<code.length)
-    		{*/
-    	
-            //str=code[i]; //+(col[i]/120);
             
             if(stpf[i][0]==0 && stpf[i][1]==0)
             {
-            	sentence="<FONT size=\"5\" STYLE=\"background-color:white\" >";
+            	int index=0;
+            	
+            	
+            	shade="#FFFFFF";
+            	while(index<code[i].length())
+            		if(code[i].charAt(index++)=='#')
+            		{
+            		
+            			shade="#A4A4A4";
+            			break;
+            		}	
+       
+            	
+            	sentence="<FONT size=\"5\" STYLE=\"background-color:"+shade+"\" >";
             	sentence+="<span class=\"dropt\" title=\""+results[i]+"\">"+code[i];
             	sentence+=" <span style=\"width:500px;\"></span>";
             	sentence+="</span></FONT><br>";
             
-            	//
-            	//sentence="<INPUT TYPE='text' SIZE='"+30+"' ALT=\"0\" value='"+str+" '  readonly><br/>";
-            	//sentence="<A onMouseOver=\"document.bgColor=\'white\'\">"+str+"</a><br>";
             }
             else
             {	
             	//System.out.println("Color value= "+col[i]);	//Brightness value= "+bright[i]);
-            shade=RangeColor.htmlColor(col[i]); //, bright[i]);
-            //out.write("<font color='"+shade+"'>"+str+"</font><br/>");
+            shade=RangeColor.htmlColor(col[i]);
+           
             sentence="<FONT size=\"5\" STYLE=\"background-color:"+shade+"\" >"; 
    
-            //sentence="<A onMouseOver=\"document.bgColor=\'"+shade+"\'\">"+str+"</a><br>";
+           
             
             sentence+="<span class=\"dropt\" title=\""+results[i]+"\">"+code[i];
         	sentence+=" <span style=\"width:500px;\"></span>";
@@ -73,22 +92,10 @@ String sentence=null;
             }        
             //System.out.println(str);
             out.write(sentence); 
-            //str.length()+
     		
         }	
     
   
-    /*out.write("<script language="JavaScript" type="text/javascript">");
-	out.write("var gDomain="www.qsstats.com";");
-	out.write("var gDcsId="dcs37pv2c00000oun93vypyva_4k6d";");
-	out.write("var gFpc="WT_FPC";");
-	out.write("var gConvert=true;");
-	out.write("var gFpcDom = "htmlgoodies.com";");
-	out.write("if ((typeof(gConvert) != "undefined") && gConvert && (document.cookie.indexOf(gFpc + "=") == -1) && (document.cookie.indexOf("WTLOPTOUT=")==-1)) {");
-	out.write("    document.write("<SCR"+"IPT TYPE='text/javascript' SRC='http"+(window.location.protocol.indexOf('https:')==0?'s':'')+"://"+gDomain+"/"+gDcsId+"/wtid.js"+"'><\/SCR"+"IPT>");");
-	out.write("}");
-	out.write("function dcsAdditionalParameters() {}");
-   out.write(" </script>");*/
     
    out.write("</pre></body></html>");
   
